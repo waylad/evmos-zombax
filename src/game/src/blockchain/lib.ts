e272d93592caa74047dfc7893c6c51d6c08894f3
waylad <waylad42@gmail.com>
2022-11-14 11:00:24 +0100

    Fix wallet connect

--- src/game/src/blockchain/lib.ts
+++ src/game/src/blockchain/lib.ts
@@ -8,16 +8,21 @@ declare var window: any
 const contractAddress = "TSyAsSP1SoUpiVi2ouaydnLFpZHo92Pkxe"
 const contractAddressHex = "41ba785befeba720d17297e14a125cc481d0c0ece3"
 let contractInstance;
+let address;
 
 export const connectWallet = async () => {
-    const address = window.tronWeb.defaultAddress.base58
+    address = window.tronWeb.defaultAddress.base58
     console.log(address)
 
-    const tronLink = await getTronLink()
-    const res: any = await tronLink?.request({ method: 'tron_requestAccounts' })
-    console.log(res)
-    
-    contractInstance = tronLink?.tronWeb.contract(ZombaxAbi.abi, contractAddress)
+    if(address) {
+      const tronLink = await getTronLink()
+      const res: any = await tronLink?.request({ method: 'tron_requestAccounts' })
+      console.log(res)
+      
+      contractInstance = tronLink?.tronWeb.contract(ZombaxAbi.abi, contractAddress)
+    } else {
+     alert("Please unlock your Tronlink Wallet ")
+    }
 
     // await contractInstance.mintCollectable(address, "https://zombax.io/assets/cars/00000000.json", "Genesis Car", 30000000, true).send({
     //   callValue: 0,
@@ -47,8 +52,7 @@ export const connectWallet = async () => {
 }
 
 export const getCars = async () => {
-  // const ownedCarsIds = await carsContractWithSigner.getTokensOwnedByMe()
-  const ownedCarsIds = await contractInstance.getTokensOwnedByMe().call()
+    const ownedCarsIds = await contractInstance.getTokensOwnedByMe().call()
   console.log('ownedCarsIds', ownedCarsIds)
   ownedCarsIds.forEach(async (ownedCarsId) => {
     // const carMeta = await carsContractWithSigner.tokenMeta(ownedCarsId)
@@ -85,8 +89,6 @@ export const buyCar = async (carToken: CarToken) => {
 }
 
 export const sellCar = async (carToken: CarToken, price: number) => {
-  // const receipt = await carsContractWithSigner.setTokenSale(carToken.tokenId, true, price)
-  // const tx = await receipt.wait()
   const tx = await contractInstance.setTokenSale(carToken.tokenId, true, price).send({
     callValue: 0,
     shouldPollResponse: true,
@@ -95,12 +97,6 @@ export const sellCar = async (carToken: CarToken, price: number) => {
 }
 
 export const upgradeCar = async (carToken: CarToken) => {
-  // const receipt = await carsContractWithSigner.updateTokenUri(
-  //   carToken.tokenId,
-  //   `https://zombax.io/assets/cars/${carToken.carCode}.json`,
-  // )
-  // const tx = await receipt.wait()
-
   const tx = await contractInstance.updateTokenUri(carToken.tokenId, `https://zombax.io/assets/cars/${carToken.carCode}.json`).send({
     callValue: 0,
     shouldPollResponse: true,
